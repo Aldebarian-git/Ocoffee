e421c6f2171332f9d8063f20dbe264bd7ea206c6
Amdebarian-git <flavien.guillonfr@gmail.com>
2025-02-13 21:35:42 +0100

    ajout de redis pour les sessions

--- index.js
+++ index.js
@@ -2,27 +2,33 @@
 // ⚠️ au début du fichier, AVANT les autres imports ⚠️
 import "dotenv/config";
 
-
-
 // Importer les dépendances
 import express from "express";
 import router from "./router.js";
 import session from "express-session";
+import RedisStore from "connect-redis";
+import { createClient } from "redis";
 
+// Connexion à Redis
+const redisClient = createClient({ url: process.env.REDIS_URL });
+redisClient.connect().catch(console.error);
 
 // Créer une app
 const app = express();
 
-app.use(session({
-  secret: 'monSecretUltraSecurisé',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: process.env.NODE_ENV === 'production', // Active secure en prod
-    httpOnly: true, // Protège contre XSS
-    sameSite: 'strict' // Empêche les attaques CSRF
-  }
-}));
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: process.env.SESSION_SECRET || "supersecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production", // Activer HTTPS en prod
+      httpOnly: true,
+      sameSite: "lax",
+    },
+  })
+);
 
 console.log("Session middleware chargé");
 
@@ -32,15 +38,13 @@ app.set("views", "views"); // préciser la localisation du dossier "views"
 
 // Configurer un dossier d'assets statiques
 // Permet de rendre accessible via leur chemin de fichier tous les fichiers présents dans le dossier "public".
-app.use(express.static( "public"));
+app.use(express.static("public"));
 
 // Ajout d'un body parser (avant le router)
 app.use(express.urlencoded({ extended: true })); // permet de récupérer les données du PAYLOAD des <form> et les ajouter à req.body
 
 // Brancher le routeur
 app.use(router);
-
-
 
 // Lancer un serveur
 const port = process.env.PORT || 3000; // Fallback (valeur par défaut) au cas où le .env ne serait pas défini
