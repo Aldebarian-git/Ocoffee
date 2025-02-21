8db7fe869f9f9b1532b0fc989122261e6cca69a8
Amdebarian-git <flavien.guillonfr@gmail.com>
2025-02-21 10:15:07 +0100

    test multer via railway volume

--- middlewares/old-multer.js
+++ middlewares/old-multer.js
@@ -2,16 +2,10 @@ import multer from "multer";
 import path from "node:path";
 import fs from "fs";
 
-// Vérifie si l'environnement est en production
-const isProduction = process.env.NODE_ENV === "production";
-
 // Configuration Multer (unique et globale)
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    // Définir le répertoire de destination en fonction de l'environnement
-    const dir = isProduction
-      ? "/mnt/data/coffees" // Répertoire pour la production
-      : path.join(process.cwd(), "public", "assets", "coffees"); // Répertoire pour le développement
+    const dir = path.join(process.cwd(), "public", "assets", "coffees");
 
     // Vérifie si le dossier existe, sinon le crée
     if (!fs.existsSync(dir)) {
@@ -23,28 +17,18 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     if (!req.body.reference) {
       return cb(
-        new Error(
-          "Le champ 'reference' est obligatoire pour nommer le fichier."
-        )
+        new Error("Le champ 'reference' est obligatoire pour nommer le fichier.")
       );
     }
 
     const fileName = `${req.body.reference}.png`;
-    const filePath = path.join(
-      isProduction
-        ? "/mnt/data/coffees"
-        : path.join(process.cwd(), "public", "assets", "coffees"),
-      fileName
-    );
+    const filePath = path.join(process.cwd(), "public", "assets", "coffees", fileName);
 
     // Vérifie si le fichier existe déjà et le supprime avant d'enregistrer le nouveau
     if (fs.existsSync(filePath)) {
       fs.unlink(filePath, (err) => {
         if (err) {
-          console.error(
-            "Erreur lors de la suppression du fichier existant :",
-            err
-          );
+          console.error("Erreur lors de la suppression du fichier existant :", err);
           return cb(new Error("Erreur lors du remplacement du fichier."));
         }
         cb(null, fileName); // Continue avec le même nom après suppression
@@ -58,3 +42,4 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 export default upload;
+
